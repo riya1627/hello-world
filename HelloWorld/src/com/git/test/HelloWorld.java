a0380b4812b8f5ccf16c227a60c0bb621e07a92e
Riya.Singharoy <Riya.Singharoy@IN2217785W2.ey.net>
2017-12-19 11:28:41 +0530

    #1 Initial/First Commit and Push

---
+++ HelloWorld/src/com/git/test/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.git.test;
+
+/**
+ * @author Riya.Singharoy
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
